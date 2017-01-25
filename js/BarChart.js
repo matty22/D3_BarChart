46830c0e1b971e2a6f846d1c042c3b85b6f83042
Matt <matt.leonard.22@gmail.com>
2017-01-25 15:53:06 -0800

    setup xhr to request dataset

--- js/BarChart.js
+++ js/BarChart.js
@@ -1,3 +1,18 @@
 // BarChart.js
 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        var json = JSON.parse(xhr.response);
+        var dataset = json.data;
+        console.log(dataset);
+    }
+    else {
+        console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+};
+xhr.send();
+
+
 d3.select("body").append("p").text("New paragraph!");
