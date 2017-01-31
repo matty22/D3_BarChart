cee93bf631fd602e8346786e9701d3166744c76f
Matt <matt.leonard.22@gmail.com>
2017-01-30 19:52:37 -0800

    played with scales a bit

--- js/BarChart.js
+++ js/BarChart.js
@@ -8,43 +8,47 @@
             var json = JSON.parse(xhr.response);
             // fulldataset contains labels and data points
             var fulldataset = json.data;
-            // dataset contains only data points
-            var dataset = [];
-            for (var i = 0; i < fulldataset.length; i ++) {
-                dataset.push(fulldataset[i][1]);
-            }
             
             function buildChart(dataset) {
-                var padding = 30;
+                var padding = 40;
                 var height = 500;
-                var width = 500;
+                var width = 884;
                 var xScale = d3.scaleLinear()
-                               .domain([0, d3.max(dataset, function(d) { return d[1][0] })])
+                               .domain([0, d3.max(dataset, function(d) {return d[1] })])
                                .range([padding, width - padding]);
-    
-    // Add your code below this line
-    
+                               
                 var yScale = d3.scaleLinear()
-                                 .domain([0, d3.max(dataset, function(d) { return d[1][1]; })])
+                                 .domain([0, d3.max(dataset, function(d) { return d[1]; })])
                                  .range([height - padding, padding]);
 
                 var svg = d3.select("#chartDiv")
                             .append("svg")
                             .attr("width", "100%")
-                            .attr("height", "500");
+                            .attr("height", height);
 
                 svg.selectAll("rect")
                    .data(dataset)
                    .enter()
                    .append("rect")
                    .attr("class", "bar")
-                   .attr("x", function(d, i) { return i * 4; })
-                   .attr("y", function(d, i) { return height - d[1] })
-                   .attr("width", "4")
+                   .attr("x", function(d, i) { return xScale(i * 50); })
+                   .attr("y", function(d, i) { return yScale(d[1]) })
+                   .attr("width", 4)
                    .attr("height", function(d, i) { return d[1]; })
                    .attr("fill", "dodgerblue")
+                   .attr("stroke", "darkblue")
                    .append("title")
-                   .text(function(d) { return d })
+                   .text(function(d) { return d });
+                
+                var xAxis = d3.axisBottom(xScale);
+                svg.append("g")
+                   .attr("transform", "translate(0," + (height - 10) + ")")
+                   .call(xAxis);
+
+                var yAxis = d3.axisLeft(yScale);
+                svg.append("g")
+                    .attr("transform", "translate(" + 40 + ",35)")
+                    .call(yAxis);
             }
             buildChart(fulldataset);
         }
